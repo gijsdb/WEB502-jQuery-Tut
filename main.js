89dc61c7134dda1f45ead8a1d04a0c540e7336ad
gijsdb <gijsdeblauw@gmail.com>
2020-08-18 10:22:38 +1200

    Commit #2

--- main.js
+++ main.js
@@ -5,3 +5,15 @@ $(document).ready(function(){
         $("p").text('Paragraph Changed...');
     });
 });
+
+$(document).ready(function(){
+    $('.change-color-button').click(function(){
+        $(".change-color").css("background-color", "red")
+    });
+});
+
+$(document).ready(function(){
+    $('.change-height-button').click(function(){
+        $(".height").css("height", "50vh")
+    });
+});
